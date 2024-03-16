8e8d8f55346762601ccfb6c163c6bd44286bd03a
abii225 <abhayv225@gmail.com>
2024-03-17 02:28:02 +0530

    alltodo and progress completed

--- src/Components/Alltodo.jsx
+++ src/Components/Alltodo.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { HiDotsHorizontal } from "react-icons/hi";
+import SingleTodo from "./SingleTodo";
+import Addnew from "./Addnew";
+const Alltodo = ({ alltodo, setTodo }) => {
+  // console.log(alltodo, "alltodo");
+  return (
+    <div className="min-h-[400px] bg-slate-200 rounded-md">
+      <div
+        key="header-alltodo"
+        className="w-[90%] mx-auto flex justify-between pt-[10px]"
+      >
+        <h1 className="text-[17px] font-primary font-semibold">To Do</h1>
+        <HiDotsHorizontal className="text-[17px]  " />
+      </div>
 
-const Alltodo = () => {
-  return <div></div>;
+      {/* ---------------------  map our todos ---------------------- */}
+      <div className="flex flex-col gap-2">
+        {alltodo.length &&
+          alltodo.map((ele) => {
+            return <SingleTodo todo={ele} setTodo={setTodo} data={alltodo} />;
+          })}
+      </div>
+      <Addnew />
+    </div>
+  );
 };
 
 export default Alltodo;
--- src/Components/Completed.jsx
+++ src/Components/Completed.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { HiDotsHorizontal } from "react-icons/hi";
+import SingleTodo from "./SingleTodo";
+import Addnew from "./Addnew";
 
-const Completed = () => {
-  return <div></div>;
+const Completed = ({ completed, setTodo }) => {
+  // console.log(deletTodo, "completed");
+  return (
+    <div className=" bg-slate-200 rounded-md">
+      <div
+        key="header-progress"
+        className="w-[90%] mx-auto flex justify-between pt-[10px]"
+      >
+        <h1 className="text-[17px] font-primary font-semibold">Done</h1>
+        <HiDotsHorizontal className="text-[17px]  " />
+      </div>
+      <div className="flex flex-col gap-2">
+        {completed.length &&
+          completed.map((ele) => {
+            return (
+              ele.status === "done" && (
+                <SingleTodo todo={ele} setTodo={setTodo} data={completed} />
+              )
+            );
+          })}
+      </div>
+      <Addnew />
+    </div>
+  );
 };
 
 export default Completed;
